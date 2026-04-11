30c705111700642f5dbe087ad04d09145a26c1f8
y-shi23 <y-shi23tsinghua@outlook.com>
2026-04-11 18:09:17 +0800

    chore: bum version to 0.2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.8"
+        versionName = "0.2.9"
 
         ndk {
             abiFilters.addAll(listOf("arm64-v8a"))
